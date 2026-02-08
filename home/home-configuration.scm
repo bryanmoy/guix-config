dde0a22605a2ebced230bfe5c654d96a0d053307
Bryan Moy <dev@bryanmoy.com>
2026-02-08 23:29:31 +0100

    feat(kubernetes): Install kubectl (#8)
    Reviewed-on: https://codeberg.org/bryanmoy/guix-config/pulls/8
    Co-authored-by: Bryan Moy <dev@bryanmoy.com>
    Co-committed-by: Bryan Moy <dev@bryanmoy.com>

--- home/home-configuration.scm
+++ home/home-configuration.scm
@@ -17,19 +17,17 @@
 (home-environment
   ;; Below is the list of packages that will show up in your
   ;; Home profile, under ~/.guix-home/profile.
-  (packages
-    (specifications->packages
-      (list
-        "chezmoi"
-        "nushell"
-        "jujutsu"
-        "git"
-        "podman"
-        "rust"
-        "rust:cargo"
-        "rust-analyzer"
-        "go@1.25.5"
-        "node")))
+  (packages (specifications->packages (list "chezmoi"
+                                            "nushell"
+                                            "jujutsu"
+                                            "git"
+                                            "podman"
+                                            "kubectl"
+                                            "rust"
+                                            "rust:cargo"
+                                            "rust-analyzer"
+                                            "go@1.25.5"
+                                            "node")))
 
   ;; Below is the list of Home services.  To search for available
   ;; services, run 'guix home search KEYWORD' in a terminal.
@@ -42,15 +40,15 @@
                                       ("ls" . "ls -p --color=auto")
                                       ("update-home" . "guix home reconfigure ~/guix-config/home/home-configuration.scm")
                                       ("update-system" . "sudo guix system reconfigure ~/guix-config/systems/stormrage.scm")))
-			   (environment-variables '(("GUIX_PACKAGE_PATH" . "$HOME/guix-config")
-						    ("EDITOR" . "emacs -nw")))))
-		 
+                           (environment-variables '(("GUIX_PACKAGE_PATH" . "$HOME/guix-config")
+                                                    ("EDITOR" . "emacs -nw")))))
+
                  ;; https://guix.gnu.org/manual/devel/en/html_node/Niri-window-manager.html
                  ;; Check %HOME/.local/share/wayland-sessions/niri.desktop for new entry after guix home reconfigure
                  (service home-niri-service-type)
                  (service home-dbus-service-type)
                  (service home-pipewire-service-type)
                  (simple-service 'flatpak-env
-                    home-environment-variables-service-type
-                    `(("XDG_DATA_DIRS" . "$XDG_DATA_DIRS:/var/lib/flatpak/exports/share:/home/$USER/.local/share/flatpak/exports/share"))))
+                                 home-environment-variables-service-type
+                                 `(("XDG_DATA_DIRS" . "$XDG_DATA_DIRS:/var/lib/flatpak/exports/share:/home/$USER/.local/share/flatpak/exports/share"))))
            %base-home-services)))
